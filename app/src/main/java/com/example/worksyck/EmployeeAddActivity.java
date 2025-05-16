f5622b45c8df5c4c0ffde09ea8a595474fee1dc6
SojoodSalahAl-deen <1212901@student.birzeit.edu>
2025-05-16 18:14:00 +0300

    updated

--- app/src/main/java/com/example/worksyck/EmployeeAddActivity.java
+++ app/src/main/java/com/example/worksyck/EmployeeAddActivity.java
@@ -27,7 +27,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 public class EmployeeAddActivity extends AppCompatActivity {
-    private EditText    normalHourCostEditText,   overTimeHourCostEditText, detectionhourCostEditText, baseSalaryEditText, editTextEmail, editTextPassword, editTextFullName, editTextPhone, editTextMacAddress;
+    private EditText    normalHourCostEditText,   overTimeHourCostEditText, detectionhourCostEditText, baseSalaryEditText, editTextEmail, editTextPassword, editTextFullName, editTextPhone;
     private RadioGroup radioGroupStatus;
         private Button btnAddEmployee;
     private RequestQueue requestQueue;
@@ -35,7 +35,7 @@ public class EmployeeAddActivity extends AppCompatActivity {
     private ArrayList<Designations> designations;
     private Spinner departmentsSpinner;
 
-    private String SalaryStructureType;
+private String salary_structure;
 
     private double base_salary;
     private double hourCost;
@@ -66,7 +66,7 @@ public class EmployeeAddActivity extends AppCompatActivity {
         editTextPhone = findViewById(R.id.editTextPhone);
         normalHourCostEditText=findViewById(R.id.normalHourCostEditText);
           overTimeHourCostEditText=findViewById(R.id.OverTimeHourCostEditText);
-        editTextMacAddress = findViewById(R.id.editTextMacAddress);
+//        editTextMacAddress = findViewById(R.id.editTextMacAddress);
         departmentsSpinner = findViewById(R.id.departments_spinner);
         designationsSpinner = findViewById(R.id.designations_spinner);
          SalaryStructureSpinner = findViewById(R.id.salary_structure_spinner);
@@ -87,24 +87,49 @@ public class EmployeeAddActivity extends AppCompatActivity {
 //        Fill the departments  && designations on the spinner
         FetchAllDepartments();
         FetchAllDesignations();
+
         String[] Salary_structure_item = {"Salary Structure","Basic Salary" ,"per Hour"};
+
+
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Salary_structure_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         SalaryStructureSpinner.setAdapter(adapter);
 
+
         SalaryStructureSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if (position == 1) {
-                    SalaryStructureType="base salary";
+//                Log.d("SpinnerSelection", "Position: " + position + ", Value: " + Salary_structure_item[position] + SalaryStructureType);
+                 if (position == 0) {
+//                     Log.d("SpinnerSelection", "Position: " + position + ", Value: " + Salary_structure_item[position] + SalaryStructureType);
+                   salary_structure="";
+//                     Log.d("SpinnerSelection", "Position: " + position + ", Value: " + Salary_structure_item[position] + SalaryStructureType);
+
+                     base_salary=0;
+                     overTimeRate=0;
+                     normalHourRate=0;
+                     hourCost=0;
+                     normalHourCostEditText.setVisibility(View.GONE);
+                     overTimeHourCostEditText.setVisibility(View.GONE);
+                     baseSalaryEditText.setVisibility(View.GONE);
+                     detectionhourCostEditText.setVisibility(View.GONE);
+                 }
+                else if (position == 1 ) {
+                    salary_structure="base salary";
                     baseSalaryEditText.setVisibility(View.VISIBLE);
                     detectionhourCostEditText.setVisibility(View.VISIBLE);
                     normalHourCostEditText.setVisibility(View.GONE);
                     overTimeHourCostEditText.setVisibility(View.GONE);
+                    overTimeRate=0;
+                    normalHourRate=0;
 
 
-                } else {
-                    SalaryStructureType="per hour";
+                }
+                else {
+                    salary_structure="per Hour";
+                    base_salary=0;
+                    hourCost=0;
+
                     overTimeHourCostEditText.setVisibility(View.VISIBLE);
                     normalHourCostEditText.setVisibility(View.VISIBLE);
                     baseSalaryEditText.setVisibility(View.GONE);
@@ -114,11 +139,7 @@ public class EmployeeAddActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                SalaryStructureType="base salary";
-                baseSalaryEditText.setVisibility(View.GONE);
-               detectionhourCostEditText.setVisibility(View.GONE);
-                baseSalaryEditText.setVisibility(View.GONE);
-                detectionhourCostEditText.setVisibility(View.GONE);
+
             }
         });
         departmentsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -130,7 +151,7 @@ public class EmployeeAddActivity extends AppCompatActivity {
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                department_id = -1;
+
             }
         });
         designationsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -143,7 +164,7 @@ public class EmployeeAddActivity extends AppCompatActivity {
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                designation_id = -1;
+
             }
         });
 
@@ -157,77 +178,73 @@ public class EmployeeAddActivity extends AppCompatActivity {
                 String password = editTextPassword.getText().toString().trim();
                 String fullName = editTextFullName.getText().toString().trim();
                 String phone = editTextPhone.getText().toString().trim();
-//                String macAddress = editTextMacAddress.getText().toString().trim();
+
                 int selectedId = radioGroupStatus.getCheckedRadioButtonId();
                 RadioButton selectedRadioButton = findViewById(selectedId);
                 selectedStatusValue = selectedRadioButton.getText().toString().trim();
 
+if (salary_structure.equals("base salary")){
+    String baseSalaryStr= baseSalaryEditText.getText().toString().trim();
+    String hourCostString  =detectionhourCostEditText.getText().toString().trim();
+    if (!baseSalaryStr.isEmpty()) {
+        try {
+            base_salary = Double.parseDouble(baseSalaryStr);
+        } catch (NumberFormatException e) {
 
-                String baseSalaryStr= baseSalaryEditText.getText().toString().trim();
-                String hourCostString  =detectionhourCostEditText.getText().toString().trim();
-                if (baseSalaryEditText.getVisibility() == View.VISIBLE) {
+            Toast.makeText(EmployeeAddActivity.this, "الرجاء إدخال رقم صالح للراتب الأساسي", Toast.LENGTH_SHORT).show();
+        }
+    }
+    else{
+        Toast.makeText(EmployeeAddActivity.this,"base salary is required", Toast.LENGTH_SHORT).show();
+    }
+    if(!hourCostString.isEmpty()){
+        try {
+            hourCost = Double.parseDouble(hourCostString);
 
-                    if (!baseSalaryStr.isEmpty()) {
-                        try {
-                            base_salary = Double.parseDouble(baseSalaryStr);
+        } catch (NumberFormatException e) {
 
-                        } catch (NumberFormatException e) {
+            Toast.makeText(EmployeeAddActivity.this, "الرجاء إدخال رقم صالح لسعر الساعه", Toast.LENGTH_SHORT).show();
+        }
 
-                            Toast.makeText(EmployeeAddActivity.this, "الرجاء إدخال رقم صالح للراتب الأساسي", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                    else{
-                        Toast.makeText(EmployeeAddActivity.this,"base salary is required", Toast.LENGTH_SHORT).show();
-                    }
+    }else{
+        Toast.makeText(EmployeeAddActivity.this,"hour Cost is required", Toast.LENGTH_SHORT).show();
+    }
 
-                    if(!hourCostString.isEmpty()){
-                        try {
-                            hourCost = Double.parseDouble(hourCostString);
+}else  if (salary_structure.equals("per Hour")) {
+    String overTimeRateSalaryStr = overTimeHourCostEditText.getText().toString().trim();
+    String normalHourRateStr = normalHourCostEditText.getText().toString().trim();
+    if (!overTimeRateSalaryStr.isEmpty()) {
+        try {
+            overTimeRate = Double.parseDouble(overTimeRateSalaryStr);
+        } catch (NumberFormatException e) {
+            Toast.makeText(EmployeeAddActivity.this, "الرجاء إدخال رقم صالح لسعر الساعه الاضافيه", Toast.LENGTH_SHORT).show();
 
-                        } catch (NumberFormatException e) {
+        }
+    } else {
+        Toast.makeText(EmployeeAddActivity.this, "over time hour Cost is required", Toast.LENGTH_SHORT).show();
 
-                            Toast.makeText(EmployeeAddActivity.this, "الرجاء إدخال رقم صالح لسعر الساعه", Toast.LENGTH_SHORT).show();
-                        }
+    }
+    if (!normalHourRateStr.isEmpty()) {
+        try {
+            normalHourRate = Double.parseDouble(normalHourRateStr);
+        } catch (NumberFormatException e) {
+            Toast.makeText(EmployeeAddActivity.this, "الرجاء إدخال رقم صالح لسعر الساعه العاديه", Toast.LENGTH_SHORT).show();
 
-                    }else{
-                        Toast.makeText(EmployeeAddActivity.this,"hour Cost is required", Toast.LENGTH_SHORT).show();
-                    }
-                } else {
-                    String overTimeRateSalaryStr = overTimeHourCostEditText.getText().toString().trim();
-                    String normalHourRateStr = normalHourCostEditText.getText().toString().trim();
-                    if (!overTimeRateSalaryStr.isEmpty()) {
-                        try {
-                            overTimeRate = Double.parseDouble(overTimeRateSalaryStr);
-                        } catch (NumberFormatException e) {
-                            Toast.makeText(EmployeeAddActivity.this, "الرجاء إدخال رقم صالح لسعر الساعه الاضافيه", Toast.LENGTH_SHORT).show();
-
-                        }
-                    } else {
-                        Toast.makeText(EmployeeAddActivity.this, "over time hour Cost is required", Toast.LENGTH_SHORT).show();
-
-                    }
-                    if (!normalHourRateStr.isEmpty()) {
-                        try {
-                            normalHourRate = Double.parseDouble(normalHourRateStr);
-                        } catch (NumberFormatException e) {
-                            Toast.makeText(EmployeeAddActivity.this, "الرجاء إدخال رقم صالح لسعر الساعه العاديه", Toast.LENGTH_SHORT).show();
-
-                        }
-                    } else {
-                        Toast.makeText(EmployeeAddActivity.this, "normal hour Cost is required", Toast.LENGTH_SHORT).show();
-
-                    }
-
-
-                }
-                Employee employee = new Employee(email, password, fullName, phone, selectedStatusValue, Integer.valueOf(company_id), department_id, designation_id,base_salary,hourCost,SalaryStructureType,normalHourRate,overTimeRate);
+        }
+    } else {
+        Toast.makeText(EmployeeAddActivity.this, "normal hour Cost is required", Toast.LENGTH_SHORT).show();
+    }
+}
+                Employee employee = new Employee(email, password, fullName, phone, selectedStatusValue, Integer.valueOf(company_id), department_id, designation_id,base_salary,hourCost,salary_structure,normalHourRate,overTimeRate);
+               Log.d("Dep_id" , String.valueOf(department_id));
+//                Log.d("Salary type" , SalaryStructureType);
                 Add_new_Employeee(employee);
-
             }
         });
     }
                                       private void Add_new_Employeee(Employee employee) {
-
+                                          Log.d("Dep_id1" , String.valueOf(department_id));
+                                          Log.d("test" , salary_structure);
 
                                           String add_new_employee_url = "http://10.0.2.2//worksync/add_employee.php";
                                           StringRequest stringRequest = new StringRequest(Request.Method.POST, add_new_employee_url, new Response.Listener<String>() {
@@ -270,16 +287,19 @@ public class EmployeeAddActivity extends AppCompatActivity {
                                                   params.put("department_id", String.valueOf(employee.getDepartmentId()));
                                                   params.put("designation_id", String.valueOf(employee.getDesignationId()));
 
-                                                  if (employee.getBaseSalary() > 0) {
+                                                  params.put("salary_structure_type", employee.getSalaryStructureType());
+                                                  if (salary_structure.equals("base salary")){
                                                       params.put("base_salary", String.valueOf(employee.getBaseSalary()));
-                                                      params.put("detection_hour_cost", String.valueOf(employee.getHourCost()));
-                                                      params.put("salary_structure_type", employee.getSalaryStructureType());
-                                                  }
-                                                  else{
+                                                      Log.d("base_salary", String.valueOf(employee.getBaseSalary()));
+                                                      params.put("hour_deduction_cost", String.valueOf(employee.getHourCost()));
+                                                  }else if(salary_structure.equals("per Hour")){
                                                       params.put("overtime_hour_rate", String.valueOf(employee.getOvertimeHourRate()));
                                                       params.put("normal_hour_rate", String.valueOf(employee.getNormalHourRate()));
-                                                      params.put("salary_structure_type", employee.getSalaryStructureType());
+
+
                                                   }
+
+//
 
 
                                                   return params;
@@ -311,6 +331,8 @@ public class EmployeeAddActivity extends AppCompatActivity {
                                                       if (status.equals("success") && !message.equals("No departments found.")) {
 
                                                           JSONArray data = jsonObject.getJSONArray("data");
+                                                          departmentNames.add("Choose Department");
+                                                          departmentIds.add(-1);
                                                           for (int i = 0; i < data.length(); i++) {
                                                               String dep_name = data.getJSONObject(i).getString("name");
                                                               String dep_id = data.getJSONObject(i).getString("id");
@@ -381,7 +403,8 @@ public class EmployeeAddActivity extends AppCompatActivity {
                                                       String status = jsonObject.getString("status");
                                                       String message = jsonObject.getString("message");
                                                       if (status.equals("success") && !message.equals("No job_titles found.")) {
-
+                                                          designationsName.add("Choose Designations");
+                                                          designationsId.add(-1);
                                                           JSONArray data = jsonObject.getJSONArray("data");
                                                           for (int i = 0; i < data.length(); i++) {
                                                               String dep_name = data.getJSONObject(i).getString("name");
